c51ad9711bbaa9f42b4d915cb46a3973cc42549f
Sathvikaan <101724183+Sathvikaan@users.noreply.github.com>
2025-03-04 00:28:01 +0530

    Add files via upload

---
+++ forloop/index.js
@@ -0,0 +1,46 @@
+// Write a for loop to print numbers from 1 to 10
+// for(var a=1;a<=10;a++){
+//     console.log(a)
+// }
+
+// Write a for loop to calculate the sum of all numbers from 1 to 100.
+// sum=0
+// for(a=1;a<=100;a++){
+//     sum+=a
+// }
+// console.log(sum)
+
+// Write a for loop to print the multiplication table of 7 (up to 10).
+// num=7;
+// for(i=1;i<=10;i++){
+//     console.log(`${num} * ${i} = ${num*i}`)
+// }
+
+// Write a for loop to print all even numbers between 1 and 20.
+// for(i=2;i<=20;i+=2){
+// console.log(i)    
+// }
+
+// Write a for loop to calculate the factorial of a number (e.g., 5!).
+// factorial=1
+// function fact(n){
+// for(i=1;i<=n;i++){
+//     factorial*=i
+// }
+// console.log(factorial)}
+// fact(5)
+
+// Write a for loop to print all odd numbers between 20 and 1 in reverse order.
+// for(i=20;i>=1;i--){
+//     console.log(i)
+// }
+
+// Write a for loop to calculate the power of a number (e.g., 2^5).
+// final=1
+// function powerFuncn(num,power){
+//     for(i=1;i<=power;i++){
+// final=final*num;
+//     }
+//     console.log(final)
+// }
+// powerFuncn(2,5)
